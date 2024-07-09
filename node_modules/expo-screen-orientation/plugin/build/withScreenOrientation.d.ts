46a744eddedcbe2d4b609d26cb0f5d36a344e45e
Thanh Lam DO <108345939+thanhlamdo915@users.noreply.github.com>
2024-07-10 00:34:05 +0700

    Delete node_modules directory

--- node_modules/expo-screen-orientation/plugin/build/withScreenOrientation.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigPlugin } from '@expo/config-plugins';
-export declare function modifyObjcAppDelegate(contents: string, mask: string): string;
-declare const _default: ConfigPlugin<void | {
-    initialOrientation?: "DEFAULT" | "ALL" | "PORTRAIT" | "PORTRAIT_UP" | "PORTRAIT_DOWN" | "LANDSCAPE" | "LANDSCAPE_LEFT" | "LANDSCAPE_RIGHT" | undefined;
-}>;
-export default _default;
